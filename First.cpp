ad73c385044afb3cb2e8f0cd755a61216cd14eda
Mayur Rohokale <114018654+mayurrohokale@users.noreply.github.com>
2023-03-09 22:12:59 +0530

    Add files via upload

---
+++ First.cpp
@@ -0,0 +1,12 @@
+#include<iostream>    // input  output stream
+
+using namespace std;   
+
+int main()
+{
+    cout<<"Jay Ganesh....\n";
+    return 0;
+}
+
+
+// g++ First.cpp -o myexe
---
+++ OOP.cpp
@@ -0,0 +1,40 @@
+// class = Characteristics + Behaviours
+// class = Data + Functions
+
+#include<iostream>
+
+using namespace std;
+
+class Demo
+{
+    public: 
+        int No1;
+        int No2;
+
+        void fun()
+        {
+            cout<<"Inside Fun\n";
+        }
+
+};
+
+int main()
+{
+    Demo obj1;
+    Demo obj2;
+
+    cout<<"Size of object : "<<sizeof(obj1)<<"\n";   
+    obj1.No1 = 10;
+    obj1.No2 = 20;
+    obj1.No2 = 30;
+
+    obj2.No1 = 30;
+    obj2.No2 = 40;
+
+    obj1.fun();
+    obj2.fun();
+    
+    cout<<obj1.No2<<"\n";
+
+    return 0;
+}
